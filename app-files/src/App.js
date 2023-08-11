4ae7e5662744ba4aec921486ece43a6f2d47ba50
codingAku <egoldferb@gmail.com>
2023-08-11 22:45:29 +0300

    see less see more function added

--- app-files/src/App.js
+++ app-files/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import Card from '@canonical/react-components/dist/components/Card/Card';
-import Strip from '@canonical/react-components/dist/components/Strip/Strip';
 
 function formatDate(dateString) {
   const options = { day: 'numeric', month: 'long', year: 'numeric' };
@@ -10,16 +9,32 @@ function formatDate(dateString) {
 
 function App() {
   const [postData, setPostData] = useState([]);
+  const [cardStates, setCardStates] = useState({});
 
   useEffect(() => {
     // Fetch data from the API
     fetch('https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json')
-      .then(response => response.json())
-      .then(data => setPostData(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
+    .then(response => response.json())
+    .then(data => {
+      // Initialize cardStates object with default values
+      const initialCardStates = {};
+      data.forEach(post => {
+        initialCardStates[post.id] = false; // Initially, "See more" content is not shown
+      });
+      setCardStates(initialCardStates);
 
-  console.log(postData);
+      // Set fetched post data
+      setPostData(data);
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}, []);
+
+  const toggleCardState = (cardId) => {
+    setCardStates(prevStates => ({
+      ...prevStates,
+      [cardId]: !prevStates[cardId]
+    }));
+  };
 
   return (
     <div className="App">
@@ -27,23 +42,20 @@ function App() {
         <div className="row">
           {postData.map(post => (
             <div className="col-4" key={post.id}>
-              <div className="p-card">
+               <div className={`p-card ${cardStates[post.id] ? 'expanded' : ''}`}>
                 <h5>CLOUD AND SERVER</h5>
                 <hr className="u"></hr>
-                <img className="p-card__image" style={{ borderRadius: "4px" }} src={post.featured_media} alt={post.title.rendered} />
+                <img className="p-card__image" style={{ borderRadius: "3px", marginTop:"10px" }} src={post.featured_media} alt={post.title.rendered} />
                 <div className="p-card__inner u-no-padding">
                   <h3><a href={post.link}>{post.title.rendered}</a></h3>
                   <h5><i>
                     By <a href={post._embedded.author[0].link}>{post._embedded.author[0].name}</a> on {formatDate(post.date)}
                   </i>
                   </h5>
+                  <hr className="u"></hr>
+                  <div dangerouslySetInnerHTML={{ __html: `<small> ${post.content.rendered} </small>` }}></div>
                 </div>
-                <hr className="u"></hr>
-                <div className="p-card__inner u-no-padding">
-                  <small>
-                    <div dangerouslySetInnerHTML={{__html: post.content.rendered}}></div>
-                  </small>
-                </div>
+                  <span onClick={() =>toggleCardState(post.id)}><small><i>{cardStates[post.id] ? "See less" : "See more"}</i></small></span>
               </div>
             </div>
           ))}
