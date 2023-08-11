5a6f2e8bc17979107b773f79832afed0d69a3d52
codingAku <egoldferb@gmail.com>
2023-08-11 14:14:44 +0300

    First card component created

--- app-files/src/App.js
+++ app-files/src/App.js
@@ -1,21 +1,28 @@
 import './App.css';
-import '@canonical/react-components/dist/components/Spinner';
+// import 'vanilla-framework/scss/_vanilla.scss'; // For Vanilla Framework
+
+import Card from '@canonical/react-components/dist/components/Card/Card';
+import Strip from '@canonical/react-components/dist/components/Strip/Strip';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div class="row">
-          <div class="col-4">
-            <div class="p-card--highlighted">
-              <div class="p-card__content">
-                <img class="p-card__image" alt="" height="185" width="330" src="https://assets.ubuntu.com/v1/36f1139e-Design-and-Web-Team-Blog.jpg"></img>
-                  <h4>
-                    <a href="#">Open Source Robotics Challenges</a>
-                  </h4>
-                  <p class="u-no-padding--bottom">Open Source Robotics Challenges is a series of blogs...</p>
+          <div class="col-5">
+            <Card className="card" title="CLOUD AND SERVER" highlighted>
+              <hr class="u-sv1"></hr>
+              <img class="p-card__image" src="https://assets.ubuntu.com/v1/0f33d832-The-State-of-Robotics.jpg"/>
+              <div class="p-card__inner">
+                <h3 style={{fontSize: "1.4em"}}>Ubuntu at KubeCon & CloudNativeCon</h3>
+                <i style={{fontSize: "0.75em"}}>By <a href='#'>Canonical</a> on 26 November 2018</i>
               </div>
-            </div>
+              <hr class="u" />
+              <div class="p-card__inner">
+                <a href="#">Article</a>
+              </div>
+            </Card>
           </div>
         </div>
       </header>
