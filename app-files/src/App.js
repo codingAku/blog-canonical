a71f8d98cadb26fbd1b6a7e1b881669d74d8160f
codingAku <egoldferb@gmail.com>
2023-08-11 12:37:20 +0300

    installed canonical react components and vanilla framework

--- app-files/src/App.js
+++ app-files/src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import '@canonical/react-components/dist/components/Spinner';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div class="row">
+          <div class="col-4">
+            <div class="p-card--highlighted">
+              <div class="p-card__content">
+                <img class="p-card__image" alt="" height="185" width="330" src="https://assets.ubuntu.com/v1/36f1139e-Design-and-Web-Team-Blog.jpg"></img>
+                  <h4>
+                    <a href="#">Open Source Robotics Challenges</a>
+                  </h4>
+                  <p class="u-no-padding--bottom">Open Source Robotics Challenges is a series of blogs...</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
